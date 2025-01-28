da38ea305165aa1bcb0a1684c8a23c268ec29cc9
Frederico Matheus de Oliveira Miraglia <fredericomiraglia@gmail.com>
2025-01-28 14:28:52 -0300

    Update Principal.py

--- Currículo/Principal.py
+++ Currículo/Principal.py
@@ -8,7 +8,7 @@ st.write('Ciência de dados | Análise de dados | Python | Power BI')
 
 col1, col2  = st.columns(2)
 with col1:
-    st.image(image='https://media.licdn.com/dms/image/v2/D4D03AQEYekEEg6TfOA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1679595559584?e=1743638400&v=beta&t=CDn8Jpp7e_GIi4KqTCPKr6JHt8yJAnCkVRmN52UKNTU',width=250, caption="Frederico Miraglia")
+    st.image(image='https://media.licdn.com/dms/image/v2/D4D03AQEHCnj_sz3DpQ/profile-displayphoto-shrink_800_800/B4DZSt8xR2HAAc-/0/1738085176914?e=1743638400&v=beta&t=2g0RyVX0xDQeHWnynoMGliWQJFzD1TL4MwB80Xk9pf4',width=250, caption="Frederico Miraglia")
 
 with col2:
     st.write('Nacionalidade: Brasileiro.')
